34511ea472a56e6d63a3fc40a464366b5c14f153
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:08:40 -0300

    Add test for VeiculoFactory vehicle creation

--- src/test/java/com/estacionamento/factory/VeiculoFactoryTest.java
+++ src/test/java/com/estacionamento/factory/VeiculoFactoryTest.java
@@ -1,1 +1,11 @@
+class VeiculoFactoryTest {
+
+    @Test
+    void deveCriarVeiculoComPlaca() {
+        VeiculoFactory factory = new VeiculoFactory();
+        Veiculo v = factory.criarVeiculo("ABC1234");
+
+        assertEquals("ABC1234", v.getPlaca());
+    }
+}
 
