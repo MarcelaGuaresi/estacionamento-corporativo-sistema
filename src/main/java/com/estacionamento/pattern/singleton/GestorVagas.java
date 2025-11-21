529723b739823d321be13387e5c79c67b8bec961
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:01:01 -0300

    Implement singleton pattern in GestorVagas class

--- src/main/java/com/estacionamento/pattern/singleton/GestorVagas.java
+++ src/main/java/com/estacionamento/pattern/singleton/GestorVagas.java
@@ -1,1 +1,19 @@
+public class GestorVagas {
+
+    private static GestorVagas instancia;
+    private List<Vaga> vagas = new ArrayList<>();
+
+    private GestorVagas() { }
+
+    public static GestorVagas getInstance() {
+        if(instancia == null) instancia = new GestorVagas();
+        return instancia;
+    }
+
+    public List<Vaga> consultarVagas() { return vagas; }
+
+    public void ocuparVaga(String placa) { }
+
+    public void liberarVaga(String placa) { }
+}
 
