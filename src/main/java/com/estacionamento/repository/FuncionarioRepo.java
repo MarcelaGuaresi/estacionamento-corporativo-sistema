dcc437ad8b9b88015eed121f9f8c54e8a6deab39
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:35:12 -0300

    Add buscarPorPlaca method to FuncionarioRepo

--- src/main/java/com/estacionamento/repository/FuncionarioRepo.java
+++ src/main/java/com/estacionamento/repository/FuncionarioRepo.java
@@ -1,5 +1,7 @@
 public class FuncionarioRepo {
     public void salvar(Funcionario f) { }
     public Funcionario buscar(String cpf) { return null; }
+    public Funcionario buscarPorPlaca(String placa) {return null;}
+
 }
 
