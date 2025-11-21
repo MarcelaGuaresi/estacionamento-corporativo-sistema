a0c128f8aec2cfc30a30a0fa065dd6d0a3d91313
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 15:59:28 -0300

    Add Reserva class with properties and constructor

--- src/main/java/com/estacionamento/domain/Reserva.java
+++ src/main/java/com/estacionamento/domain/Reserva.java
@@ -1,1 +1,35 @@
+package com.estacionamento.domain;
+
+import java.util.Date;
+
+public class Reserva {
+
+    private Vaga vaga;
+    private Funcionario funcionario;
+    private Date dataInicio;
+    private Date dataFim;
+
+    public Reserva(Vaga vaga, Funcionario funcionario, Date dataInicio, Date dataFim) {
+        this.vaga = vaga;
+        this.funcionario = funcionario;
+        this.dataInicio = dataInicio;
+        this.dataFim = dataFim;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public Date getDataInicio() {
+        return dataInicio;
+    }
+
+    public Date getDataFim() {
+        return dataFim;
+    }
+}
 
