57a8444947a5330b7632c9e28b48dfff66ec68e0
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:02:41 -0300

    Implement calcular method in CalculoMensalista class

--- src/main/java/com/estacionamento/pattern/strategy/CalculoMensalista.java
+++ src/main/java/com/estacionamento/pattern/strategy/CalculoMensalista.java
@@ -1,1 +1,7 @@
+public class CalculoMensalista implements ICalculoCobranca {
+    @Override
+    public double calcular(double valorHora, double horas) {
+        return valorHora * horas * 0.7; // exemplo
+    }
+}
 
