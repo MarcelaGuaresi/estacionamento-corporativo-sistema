417a65ac420eb2ad2a2b0c1c7d8d89e70a2bec26
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:06:41 -0300

    Add RegistroEntradaService class with access validation

--- src/main/java/com/estacionamento/service/RegistroEntradaService.java
+++ src/main/java/com/estacionamento/service/RegistroEntradaService.java
@@ -1,1 +1,12 @@
+public class RegistroEntradaService {
+
+    private FuncionarioRepo funcionarioRepo;
+    private VisitanteRepo visitanteRepo;
+    private MovimentacaoRepo movimentacaoRepo;
+    private VeiculoRepo veiculoRepo;
+
+    public boolean validarAcesso(String placa) { return true; }
+
+    public void registrarEntrada(String placa) { }
+}
 
