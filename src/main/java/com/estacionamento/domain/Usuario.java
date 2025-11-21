f6ba7b87f98cbb321d041faa623a7b9f7b1af275
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 15:54:16 -0300

    Add abstract Usuario class with nome and cpf fields

--- src/main/java/com/estacionamento/domain/Usuario.java
+++ src/main/java/com/estacionamento/domain/Usuario.java
@@ -1,1 +1,8 @@
+public abstract class Usuario {
+    protected String nome;
+    protected String cpf;
+
+    public String getNome() { return nome; }
+    public String getCpf() { return cpf; }
+}
 
