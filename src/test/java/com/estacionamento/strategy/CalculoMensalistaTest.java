fff6474e1aa0f3c2f9d08256071573650c1912a7
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:07:54 -0300

    Add test for CalculoMensalista calculation

--- src/test/java/com/estacionamento/strategy/CalculoMensalistaTest.java
+++ src/test/java/com/estacionamento/strategy/CalculoMensalistaTest.java
@@ -1,1 +1,10 @@
+class CalculoMensalistaTest {
+
+    @Test
+    void deveCalcularValorMensalista() {
+        ICalculoCobranca calc = new CalculoMensalista();
+        double result = calc.calcular(10, 5);
+        assertEquals(35.0, result);
+    }
+}
 
