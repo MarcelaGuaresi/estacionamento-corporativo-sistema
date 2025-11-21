35b762a8e5e59c669b2ac36205760bba5a03b943
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:27:20 -0300

    Refactor GestorVagas for vacancy management

--- src/main/java/com/estacionamento/pattern/singleton/GestorVagas.java
+++ src/main/java/com/estacionamento/pattern/singleton/GestorVagas.java
@@ -1,19 +1,59 @@
+package com.estacionamento.pattern.singleton;
+
+import com.estacionamento.domain.Vaga;
+import com.estacionamento.domain.StatusVaga;
+import com.estacionamento.domain.Veiculo;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorVagas {
 
     private static GestorVagas instancia;
+
     private List<Vaga> vagas = new ArrayList<>();
 
     private GestorVagas() { }
 
     public static GestorVagas getInstance() {
-        if(instancia == null) instancia = new GestorVagas();
+        if (instancia == null) {
+            instancia = new GestorVagas();
+        }
         return instancia;
     }
 
-    public List<Vaga> consultarVagas() { return vagas; }
+    public Vaga buscarVagaLivre() {
+        for (Vaga v : vagas) {
+            if (v.getStatus() == StatusVaga.LIVRE) {
+                return v;
+            }
+        }
+        return null; 
+    }
 
-    public void ocuparVaga(String placa) { }
 
-    public void liberarVaga(String placa) { }
+    public boolean ocuparVaga(Veiculo veiculo) {
+        Vaga vagaLivre = buscarVagaLivre();
+        if (vagaLivre == null) {
+            return false; // não há vagas
+        }
+        vagaLivre.ocupar(veiculo);
+        return true;
+    }
+
+    public void liberarVaga(String placa) {
+        for (Vaga v : vagas) {
+            if (v.getVeiculo() != null && v.getVeiculo().getPlaca().equals(placa)) {
+                v.liberar();
+            }
+        }
+    }
+
+    public List<Vaga> consultarVagas() {
+        return vagas;
+    }
+
+
+    public void adicionarVaga(Vaga v) {
+        vagas.add(v);
+    }
 }
-
