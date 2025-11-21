b938a515d46da11724a40f5b8146667358aab8d7
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:01:24 -0300

    Implement singleton pattern in IntegracaoRH class

--- src/main/java/com/estacionamento/pattern/singleton/IntegracaoRH.java
+++ src/main/java/com/estacionamento/pattern/singleton/IntegracaoRH.java
@@ -1,1 +1,14 @@
+public class IntegracaoRH {
+
+    private static IntegracaoRH instancia;
+
+    private IntegracaoRH() { }
+
+    public static IntegracaoRH getInstance() {
+        if(instancia == null) instancia = new IntegracaoRH();
+        return instancia;
+    }
+
+    public void enviarDados(Funcionario f, Date ent, Date sai) { }
+}
 
