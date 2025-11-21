0631d5fc5a485d34d4aeb83ff2bfd1fdece144ab
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:49:44 -0300

    Update GestorVagasTest to check first free spot

--- src/test/java/com/estacionamento/singleton/GestorVagasTest.java
+++ src/test/java/com/estacionamento/singleton/GestorVagasTest.java
@@ -1,11 +1,26 @@
+package com.estacionamento.pattern.singleton;
+
+import com.estacionamento.domain.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class GestorVagasTest {
 
     @Test
-    void deveRetornarAMesmaInstancia() {
-        GestorVagas g1 = GestorVagas.getInstance();
-        GestorVagas g2 = GestorVagas.getInstance();
+    void deveOcuparPrimeiraVagaLivre() {
 
-        assertSame(g1, g2);
+        GestorVagas gestor = GestorVagas.getInstance();
+
+        Vaga v1 = new Vaga(1);
+        Vaga v2 = new Vaga(2);
+
+        gestor.adicionarVaga(v1);
+        gestor.adicionarVaga(v2);
+
+        Veiculo veiculo = new Veiculo("ABC1234");
+
+        assertTrue(gestor.ocuparVaga(veiculo));
+        assertEquals(StatusVaga.OCUPADA, v1.getStatus());
     }
 }
-
