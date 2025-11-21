45c1ddb9c8333c3c8541267973509b687e535023
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:09:05 -0300

    Add test for singleton instance of GestorVagas
    Added a test to verify singleton instance behavior.

--- src/test/java/com/estacionamento/singleton/GestorVagasTest.java
+++ src/test/java/com/estacionamento/singleton/GestorVagasTest.java
@@ -1,1 +1,11 @@
+class GestorVagasTest {
+
+    @Test
+    void deveRetornarAMesmaInstancia() {
+        GestorVagas g1 = GestorVagas.getInstance();
+        GestorVagas g2 = GestorVagas.getInstance();
+
+        assertSame(g1, g2);
+    }
+}
 
