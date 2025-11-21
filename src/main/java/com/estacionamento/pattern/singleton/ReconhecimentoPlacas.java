22f998b6b207288a92ae553bdcc88d8118c69268
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:01:44 -0300

    Update ReconhecimentoPlacas.java

--- src/main/java/com/estacionamento/pattern/singleton/ReconhecimentoPlacas.java
+++ src/main/java/com/estacionamento/pattern/singleton/ReconhecimentoPlacas.java
@@ -1,1 +1,13 @@
+public class ReconhecimentoPlacas implements ICameraObserver {
+
+    private static ReconhecimentoPlacas instancia;
+
+    public static ReconhecimentoPlacas getInstance() {
+        if(instancia == null) instancia = new ReconhecimentoPlacas();
+        return instancia;
+    }
+
+    @Override
+    public void notificar(String placa, Image img) { }
+}
 
