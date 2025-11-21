c8bde2a889ca3a8661238164b0a0f2ee43fc88cf
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:36:22 -0300

    Add placeholder comment in registrar method

--- src/main/java/com/estacionamento/repository/MovimentacaoRepo.java
+++ src/main/java/com/estacionamento/repository/MovimentacaoRepo.java
@@ -1,5 +1,9 @@
 public class MovimentacaoRepo {
     public void registrar(Movimentacao m) { }
     public List<Movimentacao> buscarHistorico(String placa) { return null; }
+    public void registrar(Movimentacao mov) {
+    // salvará futuramente
+}
+
 }
 
