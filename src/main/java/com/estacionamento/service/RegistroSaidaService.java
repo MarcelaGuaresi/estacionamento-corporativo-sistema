07861a526c97eadbc016ce3f5ef35cc2815f052e
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:07:02 -0300

    Add RegistroSaidaService with initial methods

--- src/main/java/com/estacionamento/service/RegistroSaidaService.java
+++ src/main/java/com/estacionamento/service/RegistroSaidaService.java
@@ -1,1 +1,13 @@
+public class RegistroSaidaService {
+
+    private MovimentacaoRepo movimentacaoRepo;
+    private VeiculoRepo veiculoRepo;
+    private CobrancaService cobrancaService;
+
+    public double calcularTempo(String placa) { return 0; }
+
+    public void registrarSaida(String placa) { }
+
+    public void gerarCobranca(String placa) { }
+}
 
