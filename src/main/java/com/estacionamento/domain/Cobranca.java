b872cae24a70e1f30aae977af641580264dec8b5
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 15:59:01 -0300

    Add Cobranca class for payment management

--- src/main/java/com/estacionamento/domain/Cobranca.java
+++ src/main/java/com/estacionamento/domain/Cobranca.java
@@ -1,1 +1,31 @@
+package com.estacionamento.domain;
+
+public class Cobranca {
+
+    private String placa;
+    private double valor;
+    private SituacaoPagamento situacao;
+
+    public Cobranca(String placa, double valor) {
+        this.placa = placa;
+        this.valor = valor;
+        this.situacao = SituacaoPagamento.ENVIADO;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public SituacaoPagamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(SituacaoPagamento situacao) {
+        this.situacao = situacao;
+    }
+}
 
