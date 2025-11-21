54dfab7fdc58acb663ac5f9335abc6c964c939d5
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 16:08:14 -0300

    Add test for CalculoAvulso calculation method

--- src/test/java/com/estacionamento/strategy/CalculoAvulsoTest.java
+++ src/test/java/com/estacionamento/strategy/CalculoAvulsoTest.java
@@ -1,1 +1,10 @@
+class CalculoAvulsoTest {
+
+    @Test
+    void deveCalcularValorAvulso() {
+        ICalculoCobranca calc = new CalculoAvulso();
+        double result = calc.calcular(10, 5);
+        assertEquals(50.0, result);
+    }
+}
 
