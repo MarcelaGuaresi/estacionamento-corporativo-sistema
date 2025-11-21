d52a1b06030aa4fe313cf44a52a6727846309c04
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-11-21 15:55:46 -0300

    Update Movimentacao.java

--- src/main/java/com/estacionamento/domain/Movimentacao.java
+++ src/main/java/com/estacionamento/domain/Movimentacao.java
@@ -1,1 +1,10 @@
+public class Movimentacao {
+    private Date horaEntrada;
+    private Date horaSaida;
+    private double duracao;
+
+    public double calcularDuracao() {
+        return duracao;
+    }
+}
 
