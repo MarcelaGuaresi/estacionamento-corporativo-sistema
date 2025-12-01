165df4392ecb723b229319fd53873a0cf6628895
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 14:48:37 -0300

    Add constructor to Usuario class
    Added constructor to Usuario class for initializing name and CPF.

--- src/main/java/com/estacionamento/domain/Usuario.java
+++ src/main/java/com/estacionamento/domain/Usuario.java
@@ -1,8 +1,15 @@
+package com.estacionamento.domain;
+
+// Classe base para Funcionário e Visitante
 public abstract class Usuario {
     protected String nome;
     protected String cpf;
 
+    public Usuario(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
     public String getNome() { return nome; }
     public String getCpf() { return cpf; }
 }
-
