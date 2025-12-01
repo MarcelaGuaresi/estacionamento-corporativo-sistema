4b5fe6a5b9a7a8074873b71613c6a6aea8cc1566
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 14:56:30 -0300

    Update IntegracaoRH.java

--- src/main/java/com/estacionamento/pattern/singleton/IntegracaoRH.java
+++ src/main/java/com/estacionamento/pattern/singleton/IntegracaoRH.java
@@ -3,7 +3,7 @@ package com.estacionamento.pattern.singleton;
 import com.estacionamento.domain.Funcionario;
 import java.util.Date;
 
-// Singleton com métodos de alerta e envio de dados para RH (RF17, RF19)
+
 public class IntegracaoRH {
 
     private static IntegracaoRH instancia;
@@ -15,16 +15,15 @@ public class IntegracaoRH {
         return instancia;
     }
 
-    // Usado pelo RegistroEntradaService para notificar problemas (RF19)
+    
     public void enviarAlerta(String msg) {
-        // Lógica de envio de log/email de alerta
+        
     }
     
-    // Versão simplificada usada pelo RegistroSaidaService (Cobrança)
+   
     public void enviarDados(Funcionario f, double valorCobrado) {
-        // Lógica: Enviar dados de valor para desconto em folha (RF17)
+        
     }
 
-    // Versão completa (UML)
     public void enviarDados(Funcionario f, Date ent, Date sai) { }
 }
