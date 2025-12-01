e2ef48acf7f9100a71066f80686bde8915cc4b12
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 15:00:06 -0300

    Update MovimentacaoRepo.java

--- src/main/java/com/estacionamento/repository/MovimentacaoRepo.java
+++ src/main/java/com/estacionamento/repository/MovimentacaoRepo.java
@@ -1,9 +1,23 @@
+package com.estacionamento.repository;
+
+import com.estacionamento.domain.Movimentacao;
+import java.util.List;
+
 public class MovimentacaoRepo {
-    public void registrar(Movimentacao m) { }
-    public List<Movimentacao> buscarHistorico(String placa) { return null; }
     public void registrar(Movimentacao mov) {
-    // salvará futuramente
-}
+       
+    }
+    
+    public void atualizar(Movimentacao mov) {
+   
+    }
 
+    public Movimentacao buscarMovimentacaoAberta(String placa) {
+    
+        return null;
+    }
+    
+    public List<Movimentacao> buscarHistorico(String placa) { 
+        return null; 
+    }
 }
-
