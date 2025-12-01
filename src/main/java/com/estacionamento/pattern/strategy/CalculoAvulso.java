9f7f5f72fa01dc28e84453877e5b5cdb53449c0d
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 14:58:33 -0300

    Update CalculoAvulso.java

--- src/main/java/com/estacionamento/pattern/strategy/CalculoAvulso.java
+++ src/main/java/com/estacionamento/pattern/strategy/CalculoAvulso.java
@@ -1,7 +1,8 @@
+package com.estacionamento.pattern.strategy;
+
 public class CalculoAvulso implements ICalculoCobranca {
     @Override
     public double calcular(double valorHora, double horas) {
         return valorHora * horas;
     }
 }
-
