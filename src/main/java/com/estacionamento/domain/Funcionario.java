441445b3f1e32059ee02119e8c0e90eebd018282
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 14:50:27 -0300

    Update Funcionario.java

--- src/main/java/com/estacionamento/domain/Funcionario.java
+++ src/main/java/com/estacionamento/domain/Funcionario.java
@@ -1,6 +1,18 @@
+package com.estacionamento.domain;
+import java.util.Date;
+
 public class Funcionario extends Usuario {
     private String telefone;
     private Date dataNascimento;
     private String funcao;
-}
 
+    public Funcionario() { super("", ""); } 
+
+    public Funcionario(String nome, String cpf, String telefone, Date dataNascimento, String funcao) {
+        super(nome, cpf); 
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.funcao = funcao;
+    }
+    
+}
