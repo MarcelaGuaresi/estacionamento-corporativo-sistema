63c55014dd1e4be2735b8842b98b7020f08cec97
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 14:49:56 -0300

    Update Vaga.java

--- src/main/java/com/estacionamento/domain/Vaga.java
+++ src/main/java/com/estacionamento/domain/Vaga.java
@@ -1,8 +1,27 @@
+package com.estacionamento.domain; 
+
 public class Vaga {
     private int numero;
     private StatusVaga status;
+    private Veiculo veiculoOcupante; 
 
-    public void ocupar(Veiculo v) { }
-    public void liberar() { }
+    public Vaga(int numero) {
+        this.numero = numero;
+        this.status = StatusVaga.LIVRE;
+        this.veiculoOcupante = null;
+    }
+    
+    public StatusVaga getStatus() { return status; }
+    public Veiculo getVeiculo() { return veiculoOcupante; }
+    public int getNumero() { return numero; }
+
+    public void ocupar(Veiculo v) { 
+        this.veiculoOcupante = v; 
+        this.status = StatusVaga.OCUPADA; 
+    }
+    
+    public void liberar() { 
+        this.veiculoOcupante = null; 
+        this.status = StatusVaga.LIVRE;
+    }
 }
-
