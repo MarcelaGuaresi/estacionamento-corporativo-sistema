cd3d8342b65f50e4fec16577b1a74e5ac4dcfdd1
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 15:00:26 -0300

    Update VeiculoRepo.java

--- src/main/java/com/estacionamento/repository/VeiculoRepo.java
+++ src/main/java/com/estacionamento/repository/VeiculoRepo.java
@@ -1,3 +1,6 @@
+package com.estacionamento.repository;
+
+import com.estacionamento.domain.Veiculo; 
 public class VeiculoRepo {
     public void salvar(Veiculo v) { }
     public Veiculo buscar(String placa) { return null; }
