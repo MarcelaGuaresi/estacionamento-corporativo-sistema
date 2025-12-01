6c6bffa73e9494954b11b0388805cc9d43b13c0f
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 15:01:08 -0300

    Update FuncionarioRepo.java

--- src/main/java/com/estacionamento/repository/FuncionarioRepo.java
+++ src/main/java/com/estacionamento/repository/FuncionarioRepo.java
@@ -1,3 +1,6 @@
+package com.estacionamento.repository;
+
+import com.estacionamento.domain.Funcionario; 
 public class FuncionarioRepo {
     public void salvar(Funcionario f) { }
     public Funcionario buscar(String cpf) { return null; }
