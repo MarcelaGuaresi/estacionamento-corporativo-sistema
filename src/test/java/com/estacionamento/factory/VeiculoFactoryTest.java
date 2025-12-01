89fecbf09acfe4aa2b8a1815a74d17a6d64eadd8
MarcelaGuaresi <marcelas.g.1806@gmail.com>
2025-12-01 15:06:29 -0300

    Update VeiculoFactoryTest.java

--- src/test/java/com/estacionamento/factory/VeiculoFactoryTest.java
+++ src/test/java/com/estacionamento/factory/VeiculoFactoryTest.java
@@ -1,11 +1,19 @@
+package com.estacionamento.factory; 
+
+import com.estacionamento.factory.VeiculoFactory; 
+import com.estacionamento.domain.Veiculo;        
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class VeiculoFactoryTest {
 
     @Test
     void deveCriarVeiculoComPlaca() {
+       
         VeiculoFactory factory = new VeiculoFactory();
         Veiculo v = factory.criarVeiculo("ABC1234");
 
+      
         assertEquals("ABC1234", v.getPlaca());
     }
 }
-
